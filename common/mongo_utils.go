8d8db723f423a6143c9d337a0547eae5f6f40560
YRadov <radov.yuriy@ukr.net>
2018-03-28 22:54:44 +0300

    add mongo utils

--- common/mongo_utils.go
+++ common/mongo_utils.go
@@ -26,3 +26,69 @@ func GetSession() *mgo.Session {
 	return session
 }//GetSession
 
+func createDBSession() {
+	var err error
+	session, err = mgo.DialWithInfo(&mgo.DialInfo{
+		Addrs: []string{AppConfig.MongoDBHost},
+		Username: AppConfig.DBUser,
+		Password: AppConfig.DBPwd,
+		Timeout: 60 * time.Second,
+	})
+	if err != nil {
+		log.Fatalf("[createDbSession]: %s\n", err)
+	}
+}//createDBSession
+
+// DataStore for MongoDB
+type DataStore struct {
+	MongoSession *mgo.Session
+}
+
+// Close closes an mgo.Session value.
+// Used to add defer statements for closing the copied session.
+func (ds *DataStore) Close() {
+	ds.MongoSession.Close()
+}
+
+// Collection returns mgo.collection for the given name
+func (ds *DataStore) Collection(name string) *mgo.Collection {
+	return ds.MongoSession.DB(AppConfig.Database).C(name)
+}
+
+// NewDataStore creates a new DataStore object to be used for each HTTP request.
+func NewDataStore() *DataStore  {
+	session := GetSession().Copy()
+	dataStore := &DataStore{
+		MongoSession: session,
+	}
+	return dataStore
+}//NewDataStore
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
